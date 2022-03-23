4ea7be2a9a39abb2a44cc8b51f39cfd779e37b8e
kazu-0226 <59648368+kazu-0226@users.noreply.github.com>
2022-03-23 11:52:52 +0900

    Initial commit

---
+++ src/components/UserCard.js
@@ -0,0 +1,14 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <dl>
+        <dt>名前</dt>
+        <dd></dd>
+        <dt>メール</dt>
+        <dd></dd>
+        <dt>住所</dt>
+        <dd></dd>
+      </dl>
+    </div>
+  );
+};
